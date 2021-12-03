99d86460f8ef45096293248cba8b7358627d0441
ecal <ecallanan04@gmail.com>
2021-12-03 17:14:58 -0500

    Add get-song script

---
+++ coeus/scripts/get-song.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Set the source audio player here.
+# Players supporting the MPRIS spec are supported.
+# Examples: spotify, vlc, chrome, mpv and others.
+# Use `playerctld` to always detect the latest player.
+# See more here: https://github.com/altdesktop/playerctl/#selecting-players-to-control
+PLAYER="spotify"
+
+# Format of the information displayed
+# Eg. {{ artist }} - {{ album }} - {{ title }}
+# See more attributes here: https://github.com/altdesktop/playerctl/#printing-properties-and-metadata
+FORMAT="{{ title }} - {{ artist }}"
+
+PLAYERCTL_STATUS=$(playerctl --player=$PLAYER status 2>/dev/null)
+EXIT_CODE=$?
+
+if [ $EXIT_CODE -eq 0 ]; then
+    STATUS=$PLAYERCTL_STATUS
+else
+    STATUS="None"
+fi
+
+case $STATUS in
+    Stopped)
+        echo "No music is playing"
+        ;;
+    Paused)
+        playerctl --player=$PLAYER metadata --format "$FORMAT"
+        ;;
+    Playing)
+        playerctl --player=$PLAYER metadata --format "$FORMAT"
+        ;;
+esac
