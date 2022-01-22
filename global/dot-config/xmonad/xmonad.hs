ca08a171fa3d6053b364f4bdffa678c9f82c1901
ecal <ecallanan04@gmail.com>
2022-01-22 15:27:22 -0500

    Restore mouse functionality

--- global/dot-config/xmonad/xmonad.hs
+++ global/dot-config/xmonad/xmonad.hs
@@ -225,14 +225,13 @@ myScratchPads = [ NS "terminal" spawnTerm findTerm manageScratchpad ]
 --------------------------------------------------------------------------------
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
     -- mod-button1, Set the window to floating mode and move by dragging
-    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w >> sendMessage Arrange))
-                                       -- >> windows W.shiftMaster))
-    -- [ ((modm, button1), (\w -> focus w >> placeFocused (underMouse (0, 0))))
+    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
+                                       >> windows W.shiftMaster))
     -- mod-button2, Raise the window to the top of the stack
     , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
     -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w >> sendMessage Arrange))
-                                       -- >> windows W.shiftMaster))
+    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
+                                       >> windows W.shiftMaster))
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
 
