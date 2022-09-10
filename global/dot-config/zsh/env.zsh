2ee8a37e1fc03bd85b2ee39b4c7ac20f8946392a
ecal <ethan@ecal.dev>
2022-09-10 13:17:02 -0400

    Correct hostname var

--- global/dot-config/zsh/env.zsh
+++ global/dot-config/zsh/env.zsh
@@ -2,7 +2,7 @@ export TERM="xterm-kitty"
 export TERMINAL="kitty"
 
 # Useful vars
-export DOTFILES=~/.dotfiles/$HOSTNAME
+export DOTFILES=~/.dotfiles/$HOST
 export STOW_DIR=~/.dotfiles
 export EDITOR=nvim
 export PILOCAL="192.168.2.186"
