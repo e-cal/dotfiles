c5fe4d256be7c09c0d990151eebd5b6876bc5c96
ecal <ethan@ecal.dev>
2023-06-26 13:32:09 -0400

    zsh: omz-like completions

--- global/dot-config/zsh/alias.zsh
+++ global/dot-config/zsh/alias.zsh
@@ -1,7 +1,3 @@
-alias ".."="cd .."
-alias "..."="cd ../.."
-alias "...."="cd ../../.."
-
 alias "ls"='COLUMNS=$((`tput cols` - (`tput cols` / 10))) exa --group-directories-first --icons'
 alias "la"="ls -a"
 alias "ll"="exa -la --group-directories-first --no-permissions --no-time --git --color=always"
@@ -75,3 +71,36 @@ alias "bat-stat"="sudo tlp-stat"
 alias "prolog"="swipl"
 
 alias "testlocm"="python ~/projects/macq/tests/extract/test_locm.py"
+
+# Working with dirs
+setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+
+alias -- -='cd -'
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
+alias md='mkdir -p'
+
+function d () {
+  if [[ -n $1 ]]; then
+    dirs "$@"
+  else
+    dirs -v | head -n 10
+  fi
+}
+compdef _dirs d
