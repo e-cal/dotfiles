8136a86ad166ccbf0d87100d4cf0a268aa753b4a
ecal <ethan@ecal.dev>
2024-03-07 15:37:30 -0500

    nixtogo: add

---
+++ nixtogo/.config/nixos/host.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+let
+  unstable = import
+    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/nixos-unstable)
+    { config = config.nixpkgs.config; };
+in
+{
+  networking.hostName = "nixtogo";
+
+  # Global
+  # environment.systemPackages = with pkgs; [ ];
+
+  # fonts.packages = with pkgs; [ ];
+
+  # users and user packages (gui)
+  # users.users.ecal = {
+  #   extraGroups = [];
+  #   shell = pkgs.zsh;
+  #   packages = with pkgs; [ ];
+  # };
+
+  # install dynamic libraries for unpackaged programs
+  # https://nix.dev/guides/faq.html
+  # programs.nix-ld.libraries = with pkgs; [ ];
+
+ # environment.sessionVariables = {
+ #    LD_LIBRARY_PATH = lib.mkForce "${pkgs.stdenv.cc.cc.lib}/lib";
+ #  };
+}
