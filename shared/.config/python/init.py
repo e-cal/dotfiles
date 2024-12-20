fca3e6a521c46a557d29f099f5a663c88418e496
ecal <ethan@ecal.dev>
2024-12-20 11:03:16 -0500

    python: fix ipy error

--- shared/.config/python/init.py
+++ shared/.config/python/init.py
@@ -9,13 +9,15 @@ try:
     readline.read_history_file(histfile)
     readline.set_history_length(10000)
 except FileNotFoundError:
-    try:
-        f = open(histfile, "w")
-        f.close()
-    except FileNotFoundError:
-        os.mkdir(histdir)
-        f = open(histfile, "w")
-        f.close()
+    os.makedirs(histdir, exist_ok=True)
+    f = open(histfile, "w")
+    f.close()
+except OSError:
+    print("Error opening history file, wiping and creating new")
+    os.makedirs(histdir, exist_ok=True)
+    f = open(histfile, "w")
+    f.close()
+
 
 
 print("Using histfile: ", histfile)
