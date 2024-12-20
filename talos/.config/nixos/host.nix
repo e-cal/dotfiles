5fcf942786dd12a7381cd1af44d285629c2eaa93
ecal <ethan@ecal.dev>
2024-12-20 11:04:11 -0500

    nixos: cleanup talos host.nix

--- talos/.config/nixos/host.nix
+++ talos/.config/nixos/host.nix
@@ -7,30 +7,11 @@ let
 in {
   networking.hostName = "talos";
 
-  # Global
-  # environment.systemPackages = with pkgs; [ ];
-
-  # fonts.packages = with pkgs; [ ];
-
-  # users and user packages (gui)
-  # users.users.ecal = {
-  #   extraGroups = [];
-  #   shell = pkgs.zsh;
-  #   packages = with pkgs; [ ];
-  # };
-
-  # install dynamic libraries for unpackaged programs
-  # https://nix.dev/guides/faq.html
-  # programs.nix-ld.libraries = with pkgs; [ ];
-
-  # environment.sessionVariables = {
-  #    LD_LIBRARY_PATH = lib.mkForce "${pkgs.stdenv.cc.cc.lib}/lib";
-  #  };
-
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
   };
+
 
 }
