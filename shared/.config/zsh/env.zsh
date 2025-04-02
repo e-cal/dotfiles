433c6dab9bdcf934e3bc9905a5e9302e707d52cf
ecal <ethan@ecal.dev>
2025-04-02 09:09:25 -0400

    zsh: better filtering on tree ls, use ls not la

--- shared/.config/zsh/env.zsh
+++ shared/.config/zsh/env.zsh
@@ -112,5 +112,6 @@ export AZURE_CONFIG_DIR=$XDG_DATA_HOME/azure
 export EM_CONFIG="$XDG_CONFIG_HOME"/emscripten/config
 export EM_CACHE="$XDG_CACHE_HOME"/emscripten/cache
 export EM_PORTS="$XDG_DATA_HOME"/emscripten/cache
+export CLAUDE_CONFIG_DIR="$XDG_CONFIG_HOME"/claude
 
 [[ -f $XDG_CONFIG_HOME/zsh/keys.zsh ]] && source $XDG_CONFIG_HOME/zsh/keys.zsh
